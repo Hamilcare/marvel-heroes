361033d41ad20f64d6adef0c4aec83aabd81e02b
vivien <27066260+vivienlouradour@users.noreply.github.com>
2020-02-18 14:31:58 +0100

    fix script import-mongo

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -15,7 +15,44 @@ const insertHeroes = (db, callback) => {
     fs.createReadStream('./all-heroes.csv')
         .pipe(csv())
         .on('data', data => {
-            heroes.push(data)
+            heroes.push({
+                id: data.id,
+                name: data.name,
+                description: data.description,
+                imageUrl: data.imageUrl,
+                backgroundImageUrl: data.backgroundImageUrl,
+                externalLink: data.externalLink,
+                identity: {
+                    secretIdentities: data.secretIdentities.split(','),
+                    birthPlace: data.birthPlace,
+                    occupation: data.occupation,
+                    aliases: data.aliases.split(','),
+                    alignment: data.alignment,
+                    firstAppearance: data.firstAppearance,
+                    yearAppearance: parseInt(data.yearAppearance),
+                    universe: data.universe
+                },
+                appearance: {
+                    gender: data.gender,
+                    race: data.race,
+                    height: parseInt(data.height),
+                    weight: parseInt(data.weight),
+                    eyeColor: data.eyeColor,
+                    hairColor: data.hairColor
+                },
+                teams: data.teams.split(','),
+                powers: data.powers.split(','),
+                partners: data.partners.split(','),
+                skills: {
+                    intelligence: parseInt(data.intelligence),
+                    strength: parseInt(data.strength),
+                    speed: parseInt(data.speed),
+                    durability: parseInt(data.durability),
+                    power: parseInt(data.power),
+                    combat: parseInt(data.combat)
+                },
+                creators: data.creators.split(',')
+            })
         })
         .on('end', () => {
             collection.insertMany(heroes, (err, result) => {
