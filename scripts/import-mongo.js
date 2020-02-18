a8b398955069ad13e08492c362cd6fefbf7f6667
vivien <27066260+vivienlouradour@users.noreply.github.com>
2020-02-18 10:43:45 +0100

    script import mongodb

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -6,6 +6,32 @@ var MongoClient = mongodb.MongoClient;
 var mongoUrl = "mongodb://localhost:27017";
 const dbName = "marvel";
 const collectionName = "heroes";
+//const bulkSize = 1000;
 
-// TODO
-console.log("TODO ;-)");
+
+const insertHeroes = (db, callback) => {
+    const collection = db.collection(collectionName)
+    let heroes = []
+    fs.createReadStream('./all-heroes.csv')
+        .pipe(csv())
+        .on('data', data => {
+            heroes.push(data)
+        })
+        .on('end', () => {
+            collection.insertMany(heroes, (err, result) => {
+                callback(result)
+            })
+        })
+}
+
+MongoClient.connect(mongoUrl, function (err, client) {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+    const db = client.db(dbName);
+    insertHeroes(db, (result) => {
+        console.log('Insertion completed : ', result)
+        client.close();
+    })
+});
